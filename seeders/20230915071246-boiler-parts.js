8ceed1ea9bec572d28342050b4028d5a5d625f31
nuraga123 <sabrabat54@gmail.com>
2023-12-22 16:28:41 +0400

    create server

--- seeders/20230915071246-boiler-parts.js
+++ seeders/20230915071246-boiler-parts.js
@@ -1,5 +1,8 @@
-const { faker } = require('@faker-js/faker');
-const moment = require('moment-timezone');
+/* eslint-disable @typescript-eslint/no-var-requires */
+// const { faker } = require('@faker-js/faker');
+// const moment = require('moment-timezone');
+import { faker } from '@faker-js/faker';
+import moment from 'moment-timezone';
 
 ('use strict');
 
@@ -29,48 +32,47 @@ const partsManufacturers = [
 ];
 
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    const utcOffset = 4 * 60;
-    return queryInterface.bulkInsert(
-      'BoilerParts',
-      [...Array(100)].map(() => {
-        const now = moment().utcOffset(utcOffset);
-        const createdAt = now.format('YYYY-MM-DD HH:mm:ss');
-        const updatedAt = now.format('YYYY-MM-DD HH:mm:ss');
+export async function up(queryInterface, Sequelize) {
+  const utcOffset = 4 * 60;
+  console.log(Sequelize);
+  return queryInterface.bulkInsert(
+    'BoilerParts',
+    [...Array(100)].map(() => {
+      const now = moment().utcOffset(utcOffset);
+      const createdAt = now.format('YYYY-MM-DD HH:mm:ss');
+      const updatedAt = now.format('YYYY-MM-DD HH:mm:ss');
 
-        return {
-          boiler_manufacturer:
-            boilerManufacturers[
-              Math.floor(Math.random() * boilerManufacturers.length)
-            ],
-          parts_manufacturer:
-            partsManufacturers[
-              Math.floor(Math.random() * partsManufacturers.length)
-            ],
-          price: faker.random.numeric(4),
-          name: faker.lorem.sentence(2),
-          description: faker.lorem.sentence(10),
-          images: JSON.stringify(
-            [...Array(7)].map(
-              () =>
-                `${faker.image.technics()}?random=${faker.random.numeric(30)}`,
-            ),
+      return {
+        boiler_manufacturer:
+          boilerManufacturers[
+            Math.floor(Math.random() * boilerManufacturers.length)
+          ],
+        parts_manufacturer:
+          partsManufacturers[
+            Math.floor(Math.random() * partsManufacturers.length)
+          ],
+        price: faker.random.numeric(4),
+        name: faker.lorem.sentence(2),
+        description: faker.lorem.sentence(10),
+        images: JSON.stringify(
+          [...Array(7)].map(
+            () =>
+              `${faker.image.technics()}?random=${faker.random.numeric(30)}`,
           ),
-          vendor_code: faker.internet.password(),
-          in_stock: faker.random.numeric(1),
-          bestseller: faker.datatype.boolean(),
-          new: faker.datatype.boolean(),
-          popularity: faker.random.numeric(3),
-          compatibility: faker.lorem.sentence(7),
-          createdAt,
-          updatedAt,
-        };
-      }),
-    );
-  },
-
-  async down(queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('BoilerParts', null, {});
-  },
-};
+        ),
+        vendor_code: faker.internet.password(),
+        in_stock: faker.random.numeric(1),
+        bestseller: faker.datatype.boolean(),
+        new: faker.datatype.boolean(),
+        popularity: faker.random.numeric(3),
+        compatibility: faker.lorem.sentence(7),
+        createdAt,
+        updatedAt,
+      };
+    }),
+  );
+}
+export async function down(queryInterface, Sequelize) {
+  console.log(Sequelize);
+  return queryInterface.bulkDelete('BoilerParts', null, {});
+}
