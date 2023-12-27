dbeeda08534f92e771201eb0fdae57ca5268b9b2
nuraga123 <sabrabat54@gmail.com>
2023-12-27 09:52:07 +0400

    fix auth guard 4 method

--- src/auth/authenticated.guard.ts
+++ src/auth/authenticated.guard.ts
@@ -1,15 +1,17 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UseGuards } from '@nestjs/common';
 
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
-  async canActivate(context: ExecutionContext) {
-    console.log('content start');
-    console.log({ ...context });
-    console.log('content end');
-
+  @UseGuards(AuthGuard('local'))
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    console.log('request.isAuthenticated()');
-    console.log(request.isAuthenticated());
-    return request.isAuthenticated();
+
+    if (request.isAuthenticated()) {
+      return true;
+    }
+
+    return false;
   }
 }
