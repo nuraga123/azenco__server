a633b3b8d9c2baf3ffd50b30aa2d2bb509e0557b
nuraga123 <sabrabat54@gmail.com>
2023-12-27 09:33:04 +0400

    fix auth guard 3 method

--- src/auth/authenticated.guard.ts
+++ src/auth/authenticated.guard.ts
@@ -3,7 +3,12 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
   async canActivate(context: ExecutionContext) {
+    console.log('content start');
+    console.log({ ...context });
+    console.log('content end');
+
     const request = context.switchToHttp().getRequest();
+    console.log('request.isAuthenticated()');
     console.log(request.isAuthenticated());
     return request.isAuthenticated();
   }
