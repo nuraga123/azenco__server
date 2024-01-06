b0a429641c9159fb01b4515858255170b5392f90
nuraga123 <sabrabat54@gmail.com>
2024-01-06 16:27:35 +0400

    fix users findUserOne

--- src/auth/session.serializer.ts
+++ src/auth/session.serializer.ts
@@ -1,16 +1,23 @@
-import { Injectable } from '@nestjs/common';
+// session.serializer.ts
 import { PassportSerializer } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
-  // Сериализация пользователя - сохранение только идентификатора
-  serializeUser(user: any, done: (err: Error, userId: string) => void) {
-    done(null, user.id); // предположим, что у вас есть поле id у пользователя
+  constructor(private readonly usersService: UsersService) {
+    super();
   }
 
-  // Десериализация пользователя - поиск пользователя по идентификатору
-  deserializeUser(payload: any, done: (err: Error, user: any) => void) {
-    console.log(payload);
-    done(null, payload);
+  serializeUser(user: any, done: (err: Error, user: any) => void): any {
+    done(null, user.userId);
+  }
+
+  async deserializeUser(
+    userId: string,
+    done: (err: Error, payload: any) => void,
+  ): Promise<any> {
+    const user = await this.usersService.findUserOne(+userId);
+    done(null, user);
   }
 }
