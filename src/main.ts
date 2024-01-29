dc513574e6aaebafac0541dc7d7f444921901460
nuraga123 <sabrabat54@gmail.com>
2024-01-29 15:08:46 +0400

    add message server working

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import * as session from 'express-session';
 import * as passport from 'passport';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder } from '@nestjs/swagger';
-import { SwaggerModule } from '@nestjs/swagger/dist';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap'); // Создаем логгер здесь
 
   // Используйте express-session
   app.use(
@@ -49,6 +50,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
+
+  // Пример вывода каждую минуту
+  setInterval(() => {
+    logger.log('Server Working !');
+  }, 60000);
 
   // Слушаем на порту 3000
   await app.listen(process.env.PORT || 3000);
