4476c968bf659b0422db901f6916702e47e7882a
nuraga123 <sabrabat54@gmail.com>
2024-02-27 15:38:23 +0400

    fix config database values

--- src/config/sql.config.ts
+++ src/config/sql.config.ts
@@ -1,13 +1,28 @@
 import { registerAs } from '@nestjs/config';
 import { Dialect } from 'sequelize';
 
+const database = {
+  host: 'sql.freedb.tech',
+  database__name: 'freedb_azenco',
+  username: 'freedb_nuraga',
+  password: '$nfGYZZ6Ats5WZ5',
+  port: 3306,
+  dialect: 'mysql',
+  SQL_LOGGING: true,
+  PORT: 3000,
+  SECRET: 'VeryHardSecRetAzenco',
+  EXPIRED_JWT: 10000,
+};
+
+const { dialect, database__name, host, port, password, username } = database;
+
 export const sqlConfig = registerAs('database', () => ({
-  dialect: <Dialect>process.env.SQL_DIALECT || 'mysql',
-  host: process.env.DATABASE_HOST,
-  port: +process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  dialect: <Dialect>process.env.SQL_DIALECT || 'mysql' || dialect,
+  host,
+  port,
+  username,
+  password: password,
+  database: database__name,
   autoLoadEntities: true,
   synchronize: true,
 }));
